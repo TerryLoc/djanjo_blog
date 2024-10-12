62ab4fd42769cb05f295d94cd62cc91b337766a0
Terence Loughran <terryloughran@gmail.com>
2024-10-12 09:22:40 +0000

    fix files

--- codestar/codestar/settings.py
+++ codestar/codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".codeinstitute-ide.net", ".herokuapp.com"]
+ALLOWED_HOSTS = ["127.0.0.1", ".codeinstitute-ide.net", "localhost", ".herokuapp.com"]
 
 
 # Application definition
