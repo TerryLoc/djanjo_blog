fcc414dc0581c403f52e50265f78e402affa901c
Terence Loughran <terryloughran@gmail.com>
2024-10-12 11:10:25 +0000

    fix css files for admin panel

--- codestar/codestar/urls.py
+++ codestar/codestar/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from blog.views import my_blog
@@ -23,3 +25,6 @@ urlpatterns = [
     path("blog/", my_blog, name="my_blog"),
     path("admin/", admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
